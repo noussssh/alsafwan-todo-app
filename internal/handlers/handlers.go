ba446ccc4bf0501ab25097331c722099c4c0fbe9
noussssh <noussssh@gmail.com>
2025-08-25 20:41:11 +0530

    testing CI

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,21 +1,21 @@
 package handlers
 
 import (
-    "database/sql"
-    "html/template"
-    "net/http"
+	"database/sql"
+	"html/template"
+	"net/http"
 )
 
 type Handler struct {
-    db *sql.DB
+	db *sql.DB
 }
 
 func NewHandler(db *sql.DB) *Handler {
-    return &Handler{db: db}
+	return &Handler{db: db}
 }
 
 func (h *Handler) HomeHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl := `
+	tmpl := `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,12 +24,11 @@ func (h *Handler) HomeHandler(w http.ResponseWriter, r *http.Request) {
     <title>Hello Go</title>
 </head>
 <body>
-    <h1>Hello Go!</h1>
+    <h1>Hello Go! Let's make it this time !</h1>
 </body>
 </html>
 `
 
-    t, _ := template.New("home").Parse(tmpl)
-    t.Execute(w, nil)
+	t, _ := template.New("home").Parse(tmpl)
+	t.Execute(w, nil)
 }
-
